df825dea65503907ec3cdfb9e0b2cff0645f1f31
Sojal Srivastava <22051895@kiit.ac.in>
2025-07-20 15:15:44 +0530

    Time: 519 ms (5.05%), Space: 20.8 MB (26.25%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            int count = 0;
+            for(int j = 0; j < nums.size(); j++) {
+                if(i != j && nums[i] == nums[j]) {
+                    count++;
+                    break;
+                }
+            }
+            if(count == 0) return nums[i]; // only occurs once
+        }
+        return -1; // should not happen
+    }
+};
