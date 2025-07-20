ede36e2981bdda478c16ff7a044d7c27a3dd78d0
Sojal Srivastava <22051895@kiit.ac.in>
2025-07-20 15:16:45 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.41%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int result = 0;
         for(int i = 0; i < nums.size(); i++) {
-            int count = 0;
-            for(int j = 0; j < nums.size(); j++) {
-                if(i != j && nums[i] == nums[j]) {
-                    count++;
-                    break;
-                }
-            }
-            if(count == 0) return nums[i]; // only occurs once
+            result ^= nums[i]; // XOR logic
         }
-        return -1; // should not happen
+        return result;
     }
 };
